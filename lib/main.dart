6f2b8ee22d290f2b3604834bf8fa64e3d662e57c
Luis Vinicius <luisvinicius99@gmail.com>
2023-12-02 22:37:01 -0300

    add theme to the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,20 @@
 import 'package:expense_tracker/models/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(122, 36, 21, 123),
+);
+
 void main() {
   runApp(
     MaterialApp(
-      theme: ThemeData(useMaterial3: true),
+      theme: ThemeData().copyWith(
+          useMaterial3: true,
+          colorScheme: kColorScheme,
+          appBarTheme: const AppBarTheme().copyWith(
+            backgroundColor: kColorScheme.onPrimaryContainer,
+            foregroundColor: kColorScheme.primaryContainer,
+          )),
       home: const Expenses(),
     ),
   );
